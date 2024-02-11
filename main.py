c6b2e90f35a980c4a5be5ba18407dd5de4a5d64a
Artyom 81d <demented668@proton.me>
2024-02-11 20:25:10 +0700

    fix to real aco

--- main.py
+++ main.py
@@ -1,136 +1,136 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Pool
 import time
 
 class AntColonyOptimization:
-    # Инициализация класса
-    def __init__(self, num_points, num_ants, evaporation_rate, pheromone_deposit, alpha, beta):
-        self.num_points = num_points  # Количество точек
-        self.num_ants = num_ants  # Количество муравьев
-        self.evaporation_rate = evaporation_rate  # Скорость испарения феромона
-        self.pheromone_deposit = pheromone_deposit  # Количество феромона, откладываемого каждым муравьем
-        self.alpha = alpha  # Вес феромонов
-        self.beta = beta  # Вес расстояния
-        self.points = np.random.rand(num_points, 2)  # Генерация случайных точек
-        self.pheromones = np.ones((num_points, num_points))  # Матрица феромонов
-        np.fill_diagonal(self.pheromones, 0)  # Обнуляем диагональ, так как муравьи не могут возвращаться в текущую точку
+    """
+    Реализация алгоритма Ant Colony Optimization, сочетающая в себе однопоточную и многопоточную реализацию
+    """
+    def __init__(self, num_points, num_ants, graph_range=100, alpha=1, beta=2, Q=100, pheromone_decay=0.1):
+        """
+        Конструктор, задаем константы и создаем случайный 2D граф
+        """
+        self.num_points = num_points
+        self.num_ants = num_ants
+        self.graph = np.random.rand(num_points, 2) * graph_range
+        self.alpha = alpha
+        self.beta = beta
+        self.Q = Q
+        self.pheromone_decay = pheromone_decay
 
-    # Расчет расстояния между двумя точками
-    def distance(self, p1, p2):
+    def _distance(self, p1, p2):
+        # Вычисление евклидова расстояния между двумя точками
         return np.linalg.norm(p1 - p2)
 
-    # Отображение графика с точками и путем
+    def _pheromone_update(self, best_path, best_distance):
+        # Обновление феромонов на лучшем пути
+        pheromone_increment = self.Q / best_distance
+        for i in range(self.num_points - 1):
+            self._pheromone_table[best_path[i], best_path[i + 1]] += pheromone_increment
+        self._pheromone_table[best_path[-1], best_path[0]] += pheromone_increment
+
+    def _pheromone_evaporation(self):
+        # Испарение феромонов с заданным коэффициентом
+        self._pheromone_table *= (1 - self.pheromone_decay)
+
     def display_graph(self, path=None):
-        plt.scatter(self.points[:,0], self.points[:,1])  # Отображение точек
-        if path:  # Если найден путь
-            for i in range(len(path)-1):
-                plt.plot([self.points[path[i],0], self.points[path[i+1],0]], [self.points[path[i],1], self.points[path[i+1],1]], 'k-')  # Отображение пути
-            plt.plot([self.points[path[-1],0], self.points[path[0],0]], [self.points[path[-1],1], self.points[path[0],1]], 'k-')  # Замыкающий отрезок пути
+        # Отображение графа с возможным оптимальным путем
+        plt.figure()
+        plt.scatter(self.graph[:, 0], self.graph[:, 1])
+        if path:
+            for i in range(len(path) - 1):
+                plt.plot([self.graph[path[i], 0], self.graph[path[i + 1], 0]],
+                         [self.graph[path[i], 1], self.graph[path[i + 1], 1]], 'k-')
+            plt.plot([self.graph[path[-1], 0], self.graph[path[0], 0]],
+                     [self.graph[path[-1], 1], self.graph[path[0], 1]], 'k-')
         plt.show()
 
-    # Вычисление пути с использованием муравьиного алгоритма
+    def _calculate_ant_path(self, ant_id):
+        visited = [False] * self.num_points
+        path = [ant_id]
+
+        for _ in range(self.num_points - 1):
+            current = path[-1]
+            visited[current] = True
+
+            probabilities = [(self._pheromone_table[current][i] ** self.alpha) * ((1 / self._distance(self.graph[current], self.graph[i])) ** self.beta)
+                             for i in range(self.num_points) if not visited[i]]
+            probabilities = [p / sum(probabilities) for p in probabilities]
+
+            next_point = np.random.choice([i for i in range(self.num_points) if not visited[i]],
+                                           p=probabilities)
+
+            path.append(next_point)
+
+        return path, sum(self._distance(self.graph[path[i]], self.graph[path[i + 1]]) for i in range(self.num_points - 1))
+
     def calculate_path(self, max_iterations):
+        """
+        Однопоточная реализация алгоритма
+        """
         best_path = None
-        best_distance = float('inf')
+        best_distance = np.inf
+        self._pheromone_table = np.ones((self.num_points, self.num_points))
 
         for _ in range(max_iterations):
-            for ant in range(self.num_ants):  # Для каждого муравья
-                visited = [0]  # Список посещенных точек, начинаем с первой точки
-                current = 0  # Текущая точка
-                unvisited = list(range(1, self.num_points))  # Список непосещенных точек
+            paths = [self._calculate_ant_path(i) for i in range(self.num_ants)]
+            paths.sort(key=lambda x: x[1])
 
-                while unvisited:  # Пока есть непосещенные точки
-                    probabilities = [((self.pheromones[current, i] ** self.alpha) * ((1/self.distance(self.points[current], self.points[i])) ** self.beta)) for i in unvisited]  # Расчет вероятностей перехода в непосещенные точки
-                    probabilities = probabilities / np.sum(probabilities)  # Нормализация вероятностей
-                    next_point = np.random.choice(unvisited, p=probabilities)  # Выбор следующей точки по вероятностям
-
-                    visited.append(next_point)  # Добавляем выбранную точку в посещенные
-                    unvisited.remove(next_point)  # Удаляем выбранную точку из непосещенных
-                    current = next_point  # Текущей точкой становится выбранная точка
-
-                path_distance = sum([self.distance(self.points[visited[i]], self.points[visited[i+1]]) for i in range(len(visited)-1)]) + self.distance(self.points[visited[-1]], self.points[visited[0]])  # Расчет длины пути
-
-                if path_distance < best_distance:  # Если длина пути лучше текущей лучшей
-                    best_distance = path_distance  # Обновляем лучшую длину пути
-                    best_path = visited  # Обновляем лучший найденный путь
-
-                for i in range(len(visited)-1):  # Обновляем феромоны на пройденном пути
-                    self.pheromones[visited[i], visited[i+1]] += self.pheromone_deposit/path_distance
-                    self.pheromones[visited[i+1], visited[i]] += self.pheromone_deposit/path_distance
-                self.pheromones[visited[-1], visited[0]] += self.pheromone_deposit/path_distance
-                self.pheromones[visited[0], visited[-1]] += self.pheromone_deposit/path_distance
-
-            self.pheromones *= (1 - self.evaporation_rate)  # Испарение феромонов
+            if paths[0][1] < best_distance:
+                best_path = paths[0][0]
+                best_distance = paths[0][1]
+                self._pheromone_update(best_path, best_distance)
+            self._pheromone_evaporation()
 
         return best_path, best_distance
 
-    def calculate_ant_path(self):
-        visited = [0]  # Список посещенных точек, начинаем с первой точки
-        current = 0  # Текущая точка
-        unvisited = list(range(1, self.num_points))  # Список непосещенных точек
-
-        while unvisited:  # Пока есть непосещенные точки
-            probabilities = [((self.pheromones[current, i] ** self.alpha) * (
-                        (1 / self.distance(self.points[current], self.points[i])) ** self.beta)) for i in
-                             unvisited]  # Расчет вероятностей перехода в непосещенные точки
-            probabilities = probabilities / np.sum(probabilities)  # Нормализация вероятностей
-            next_point = np.random.choice(unvisited, p=probabilities)  # Выбор следующей точки по вероятностям
-
-            visited.append(next_point)  # Добавляем выбранную точку в посещенные
-            unvisited.remove(next_point)  # Удаляем выбранную точку из непосещенных
-            current = next_point  # Текущей точкой становится выбранная точка
-
-        path_distance = sum([self.distance(self.points[visited[i]], self.points[visited[i + 1]]) for i in
-                             range(len(visited) - 1)]) + self.distance(self.points[visited[-1]],
-                                                                       self.points[visited[0]])  # Расчет длины пути
-
-        for i in range(len(visited) - 1):  # Обновляем феромоны на пройденном пути
-            self.pheromones[visited[i], visited[i + 1]] += self.pheromone_deposit / path_distance
-            self.pheromones[visited[i + 1], visited[i]] += self.pheromone_deposit / path_distance
-        self.pheromones[visited[-1], visited[0]] += self.pheromone_deposit / path_distance
-        self.pheromones[visited[0], visited[-1]] += self.pheromone_deposit / path_distance
-
-        self.pheromones *= (1 - self.evaporation_rate)  # Испарение феромонов
-        return visited, path_distance
-
     def calculate_path_parallel(self, max_iterations):
+        """
+        Многопоточная реализация алгоритма
+        """
         best_path = None
-        best_distance = float('inf')
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.calculate_ant_path) for _ in range(max_iterations)]
-            for future in futures:
-                path, distance = future.result()
-                if distance < best_distance:
-                    best_distance = distance
-                    best_path = path
+        best_distance = np.inf
+        self._pheromone_table = np.ones((self.num_points, self.num_points))
+
+        with Pool() as pool:
+            for _ in range(max_iterations):
+                paths = pool.map(self._calculate_ant_path, range(self.num_ants))
+                paths.sort(key=lambda x: x[1])
+
+                if paths[0][1] < best_distance:
+                    best_path = paths[0][0]
+                    best_distance = paths[0][1]
+                    self._pheromone_update(best_path, best_distance)
+                self._pheromone_evaporation()
+
         return best_path, best_distance
 
 if __name__ == "__main__":
     num_points = 20
-    num_ants = 10
-    evaporation_rate = 0.1
-    pheromone_deposit = 10
-    alpha = 1
-    beta = 5
+    num_ants = 20
+    max_iterations = 100
 
-    aco = AntColonyOptimization(num_points, num_ants, evaporation_rate, pheromone_deposit, alpha, beta)
-
+    aco = AntColonyOptimization(num_points, num_ants)
     aco.display_graph()
 
     start_time1 = time.time()
-    best_path1, best_distance1 = aco.calculate_path(100)
+    best_path, best_distance = aco.calculate_path(max_iterations)
+    print(f"Best path: {best_path}")
+    print(f"Best distance: {best_distance}")
     end_time1 = time.time()
-
     print(f"Time taken: {end_time1 - start_time1} seconds")
-    print(f"Distance: {best_distance1}")
-    aco.display_graph(best_path1)
+    aco.display_graph(best_path)
+
+    print()
 
     start_time2 = time.time()
-    best_path2, best_distance2 = aco.calculate_path_parallel(100)
+    best_path_parallel, best_distance_parallel = aco.calculate_path_parallel(max_iterations)
+    print(f"Best path (parallel): {best_path_parallel}")
+    print(f"Best distance (parallel): {best_distance_parallel}")
     end_time2 = time.time()
+    print(f"Time taken (parallel): {end_time2 - start_time2} seconds")
+    aco.display_graph(best_path_parallel)
 
-    print(f"Time taken async: {end_time2 - start_time2} seconds")
-    print(f"Distance: {best_distance2}")
-    aco.display_graph(best_path2)
 
+    best_path1, best_distance1 = aco.calculate_path(100)
